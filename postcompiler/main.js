4ebbc43972767277a3ab4b085223c5c07b76ced8
WillYip716 <william.yip.716@gmail.com>
2020-09-12 14:06:24 -0700

    encoding update

--- postcompiler/main.js
+++ postcompiler/main.js
@@ -9,7 +9,7 @@ function compilepost(){
     let date = "Date.now()";
     let category =  "'"+document.getElementById("category").value+"'";
     let tags = "["+document.getElementById("tags").value+"]";
-    let article = "'"+encodeURIComponent(document.getElementById("article").value)+"'";
+    let article = "'"+escape(document.getElementById("article").value)+"'";
     let imageUrl = "'"+document.getElementById("imageUrl").value+"'";
     let published = "false";
 
