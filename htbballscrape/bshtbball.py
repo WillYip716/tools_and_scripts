41deca39ffb862deb377cd9e5532c021b318e788
WillYip716 <william.yip.716@gmail.com>
2021-09-11 22:07:36 -0700

    more scraping tests

--- htbballscrape/bshtbball.py
+++ htbballscrape/bshtbball.py
@@ -10,9 +10,16 @@ league_table = soup.find('table', id = 'ContentPlaceHolder1_GridView1')
 
 
 for player in league_table.find_all('tr'):
-    for cells in player:
+    stats = player.find_all('td')
+    if len(stats):
+
+        print(stats[6].find('span').string)
+        print(stats[7].find('span').string)
+        print(stats[8].find('span').string)
+        print(stats[9].find('span').string)
+        print(stats[10].find('span').string)
+
+    """for cells in player:
         #pl_team = row.find('td', class_ ='standing-table__cell standing-table__cell--name')
         #pl_team = pl_team['data-long-name']
-        points = cells
-        #print(pl_team, points)
-        print(points)
+        print()"""
